b510e10886accdcc72058491acf3d3e74568f5c5
Silvio <70536371+Silvio-Br@users.noreply.github.com>
2025-06-22 15:21:40 +0200

    fix: drag and drop a ticket in a non-empty column (#6)
    * fix: disable text selection on ticket
    
    * fix: drag ticket in a non-empty column

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm m-2",
+      "rounded-lg border bg-card text-card-foreground shadow-sm m-2 select-none",
       className
     )}
     {...props}
